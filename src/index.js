c1eac066882373b5363c9ab3ac029ca463eb488a
kumavis <aaron@kumavis.me>
2018-01-08 14:27:16 -0800

    add http error code handling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const createAsyncMiddleware = require('json-rpc-engine/src/createAsyncMiddleware')
+const JsonRpcError = require('json-rpc-error')
 
 module.exports = createInfuraMiddleware
 module.exports.fetchConfigFromReq = fetchConfigFromReq
@@ -8,6 +9,23 @@ function createInfuraMiddleware({ network = 'mainnet' }) {
     const { fetchUrl, fetchParams } = fetchConfigFromReq({ network, req })
     const response = await fetch(fetchUrl, fetchParams)
     const rawData = await response.text()
+    // handle errors
+    if (!response.ok) {
+      switch (response.status) {
+        case 405:
+          throw new JsonRpcError.MethodNotFound()
+
+        case 418:
+          throw createRatelimitError()
+
+        case 503:
+        case 504:
+          throw createTimeoutError()
+
+        default:
+          throw createInternalError(rawData)
+      }
+    }
     // special case for now
     if (req.method === 'eth_getBlockByNumber' && rawData === 'Not Found') {
       res.result = null
@@ -39,4 +57,21 @@ function fetchConfigFromReq({ network, req }) {
   }
 
   return { fetchUrl, fetchParams }
+}
+
+
+function createRatelimitError () {
+  let msg = `Request is being rate limited.`
+  return createInternalError(msg)
+}
+
+function createTimeoutError () {
+  let msg = `Gateway timeout. The request took too long to process. `
+  msg += `This can happen when querying logs over too wide a block range.`
+  return createInternalError(msg)
+}
+
+function createInternalError (msg) {
+  const err = new Error(msg)
+  return new JsonRpcError.InternalError(err)
 }
