600e922c67372d2b3e0200f694fe0d59f48c5c97
Jenny Pollack <jennypollack3@gmail.com>
2019-01-17 14:39:23 -0800

    Revert "Add source header"

--- test/index.js
+++ test/index.js
@@ -9,13 +9,9 @@ test('fetchConfigFromReq - basic', (t) => {
     params: ['0x482103', true],
   }
 
-  const { fetchUrl, fetchParams } = fetchConfigFromReq({ network, req, source:'eth-json-rpc-infura' })
+  const { fetchUrl, fetchParams } = fetchConfigFromReq({ network, req })
   t.equals(fetchUrl, 'https://api.infura.io/v1/jsonrpc/mainnet/eth_getBlockByNumber?params=%5B%220x482103%22%2Ctrue%5D')
-  t.deepEquals(fetchParams, { 
-    method: 'GET',     
-    headers: {
-    'eth-json-rpc-infura': 'internal'
-  }, })
+  t.deepEquals(fetchParams, { method: 'GET' })
   t.end()
 
 })
@@ -28,14 +24,13 @@ test('fetchConfigFromReq - basic', (t) => {
     params: ['0x0102030405060708090a0b0c0d0e0f'],
   }
 
-  const { fetchUrl, fetchParams } = fetchConfigFromReq({ network, req, source:'eth-json-rpc-infura' })
+  const { fetchUrl, fetchParams } = fetchConfigFromReq({ network, req })
   t.equals(fetchUrl, 'https://api.infura.io/v1/jsonrpc/ropsten')
   t.deepEquals(fetchParams, {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
-      'eth-json-rpc-infura': 'internal'
     },
     body: JSON.stringify(req),
   })
@@ -53,34 +48,10 @@ test('fetchConfigFromReq - strip non-standard keys', (t) => {
     origin: 'happydapp.eth',
   }
 
-  const { fetchUrl, fetchParams } = fetchConfigFromReq({ network, req, source:'eth-json-rpc-infura' })
+  const { fetchUrl, fetchParams } = fetchConfigFromReq({ network, req })
   t.equals(fetchUrl, 'https://api.infura.io/v1/jsonrpc/ropsten')
   const parsedReq = JSON.parse(fetchParams.body)
   t.notOk('origin' in parsedReq, 'non-standard key removed from req')
-  t.end()
-
-})
-
-test('fetchConfigFromReq - request origin in header', (t) => {
-
-  const network = 'ropsten'
-  const req = {
-    method: 'eth_sendRawTransaction',
-    params: ['0x0102030405060708090a0b0c0d0e0f'],
-    origin: 'happydapp.eth',
-  }
-
-  const { fetchUrl, fetchParams } = fetchConfigFromReq({ network, req, source:'eth-json-rpc-infura' })
-  t.equals(fetchUrl, 'https://api.infura.io/v1/jsonrpc/ropsten')
-  t.deepEquals(fetchParams, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'eth-json-rpc-infura': 'happydapp.eth'
-    },
-    body: fetchParams.body,
-  })
   t.end()
 
 })
