898f24b59b5fabb3aeaed7242e961cb7eb54e11a
Elliot Winkler <elliot.winkler@gmail.com>
2022-04-06 10:18:28 -0600

    As usual, the simpler, the better

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,21 +7,6 @@ module.exports = {
     {
       files: ['*.js'],
       extends: ['@metamask/eslint-config-nodejs'],
-      parserOptions: {
-        sourceType: 'module',
-      },
-      rules: {
-        // This rule is not designed to be run against a module that uses
-        // CommonJS-style imports.
-        'import/unambiguous': 'off',
-      },
-    },
-
-    {
-      files: ['.eslintrc.js', '.prettierrc.js'],
-      parserOptions: {
-        sourceType: 'script',
-      },
     },
   ],
 
