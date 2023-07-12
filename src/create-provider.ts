d9c119981f1e1e6720f4814da2805c03d8689118
legobt <6wbvkn0j@anonaddy.me>
2023-07-12 11:19:39 +0000

    deps: replace eth-json-rpc-middleware with @metamask/eth-json-rpc-providerA

--- src/create-provider.ts
+++ src/create-provider.ts
@@ -1,6 +1,6 @@
 import { JsonRpcEngine } from 'json-rpc-engine';
-import { providerFromEngine } from 'eth-json-rpc-middleware';
-import type { SafeEventEmitterProvider } from 'eth-json-rpc-middleware';
+import { providerFromEngine } from '@metamask/eth-json-rpc-provider';
+import type { SafeEventEmitterProvider } from '@metamask/eth-json-rpc-provider';
 import {
   createInfuraMiddleware,
   CreateInfuraMiddlewareOptions,
@@ -8,13 +8,13 @@ import {
 
 /**
  * Creates a provider (as defined in
- * [`eth-json-rpc-middleware`](https://github.com/MetaMask/eth-json-rpc-middleware)
+ * [`eth-json-rpc-provider`](https://github.com/MetaMask/eth-json-rpc-provider)
  * which is preloaded with middleware specialized for interfacing with Infura
  * JSON-RPC endpoints.
  *
  * @param opts - Options to {@link createInfuraMiddleware}.
  * @returns The provider as returned by `providerFromEngine` (a part of
- * [`eth-json-rpc-middleware`](https://github.com/MetaMask/eth-json-rpc-middleware)).
+ * [`eth-json-rpc-provider`](https://github.com/MetaMask/eth-json-rpc-provider)).
  */
 export function createProvider(
   opts: CreateInfuraMiddlewareOptions,
